6c8cf1806532030e8248780f50b5698aca3dfdc8
sjai0003 <shubham.jaiswal@synchronoss.com>
2021-01-27 01:37:23 +0530

    Initial Project Commits

---
+++ src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,12 @@
+package guru.springframework.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+}
